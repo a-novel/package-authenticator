336c12f23d574361365cacf62dfaed9eb3f064fc
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-23 11:43:14 +0200

    fix(storybook): config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,14 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-onboarding", "@storybook/addon-docs"],
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {
+      builder: {
+        viteConfigPath: "./vite.storybook.config.ts",
+      },
+    },
   },
   staticDirs: [],
 };
