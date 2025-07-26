20d0494be3db2e29787d36201c211f6b610fd999
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-26 07:01:12 +0200

    fix(renovate): post upgrade tasks

--- scripts/post-upgrade-renovate.sh
+++ scripts/post-upgrade-renovate.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-echo "${#GITHUB_TOKEN}"
+# Post upgrade actions are executed after the renovate update, which uses its own configuration to pull
+# private dependencies. If we want the format command (and its required install) to succeed, we need
+# to redo the whole configuration manually.
+#
+# MAKE SURE `.npmrc` IS EXCLUDED FROM THE POST UPGRADE ACTION FILE FILTERS.
 echo "@a-novel:registry=https://npm.pkg.github.com" > "$PWD/.npmrc"
 echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> "$PWD/.npmrc"
 
