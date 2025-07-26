12431c0fdf780c2226021b90fed03cfd99f2ad27
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-26 06:33:28 +0200

    fix(renovate): post upgrade tasks

--- scripts/post-upgrade-renovate.sh
+++ scripts/post-upgrade-renovate.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-pnpm config set "@a-novel:registry" "https://npm.pkg.github.com"
-pnpm config set "//npm.pkg.github.com/:_authToken" "${RENOVATE_NPM_NPM_PKG_GITHUB_COM_TOKEN}"
+echo "@a-novel:registry=https://npm.pkg.github.com" > "$PWD/.npmrc"
+echo "//npm.pkg.github.com/:_authToken=${RENOVATE_NPM_NPM_PKG_GITHUB_COM_TOKEN}" >> "$PWD/.npmrc"
 
 pnpm i --frozen-lockfile
 pnpm format
