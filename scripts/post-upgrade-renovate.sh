03c739242592bd5e0281df7218b90debf30d745c
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-26 06:45:03 +0200

    fix(renovate): post upgrade tasks

--- scripts/post-upgrade-renovate.sh
+++ scripts/post-upgrade-renovate.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "${#RENOVATE_NPM_NPM_PKG_GITHUB_COM_TOKEN} "
 echo "@a-novel:registry=https://npm.pkg.github.com" > "$PWD/.npmrc"
 echo "//npm.pkg.github.com/:_authToken=${RENOVATE_NPM_NPM_PKG_GITHUB_COM_TOKEN}" >> "$PWD/.npmrc"
 
